f5b65b70edddc63ae1047242a25d8c9355a94ba1
Yuichiroh Arai <yuichiroh.arai@gmail.com>
2026-01-22 18:59:10 +0900

    Add GitHub Actions workflow to deploy to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,23 @@
 // @ts-check
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import { readFile } from "node:fs/promises";
+
+const pkg = JSON.parse(await readFile("./package.json", "utf-8"));
+
+const NAME = pkg.name;
+const VERSION = pkg.version;
+
 // https://astro.build/config
 export default defineConfig({
+  base: `/${NAME}/${VERSION}`,
   devToolbar: {
     enabled: false,
   },
   server: {
     host: true,
   },
+  site: process.env.ASTRO_SITE || "http://localhost:4321",
   vite: {
     plugins: [tailwindcss()],
   },
